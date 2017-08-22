b6c98d6ad6b2f4b8aa4e08bb3692060c02311cd7
Andy C. K <andzandz@users.noreply.github.com>
2017-08-22 23:15:22 +0100

    Added key overwriting

--- AnDict/AnDict.py
+++ AnDict/AnDict.py
@@ -3,12 +3,26 @@ class AnDict:
         self.items = []
 
     def put(self, key, value):
+        item = self._find_item(key)
+        if item:
+            item[1] = value
+            return
+
         self.items.append([key, value])
 
     def get(self, needle):
-        for item in self.items:
-            if item[0] == needle:
-                return item[1]
+        item = self._find_item(needle)
+        if item:
+            return item[1]
 
     def size(self):
         return len(self.items)
+
+    ## Internal methods ##
+
+    def _find_item(self, needle):
+        for item in self.items:
+            if item[0] == needle:
+                return item
+
+        return None
